54a8dd9d84049c9194031ef341f5b6a1122d6a9f
Jeremiah Lutz <bullfrog245@gmail.com>
2013-12-19 11:38:34 -0500

    Added GitHub Plugin URI for automatic WordPress updates

--- bf-krumo.php
+++ bf-krumo.php
@@ -8,6 +8,7 @@
  * Author URI:   http://www.integritive.com
  * License:      GPL-2.0+
  * License URI:  http://www.gnu.org/licenses/gpl-2.0.txt
+ * GitHub Plugin URI: https://github.com/Bullfrog245/bf-krumo
  */
 
 /**
